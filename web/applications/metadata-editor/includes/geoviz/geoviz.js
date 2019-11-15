d5f734fd78380d3f7baa37a0d52545ce8e678350
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2019-11-15 12:26:03 -0600

    Moved GML/WKT api to production API.

--- web/applications/metadata-editor/includes/geoviz/geoviz.js
+++ web/applications/metadata-editor/includes/geoviz/geoviz.js
@@ -745,7 +745,7 @@ function GeoViz()
     this.gmlToWKT = function (GML)
     {
         jQuery.ajax({
-            url: "includes/geoviz/gmltowkt.php", // replace this url with geoviz lib
+            url: "pelagos-symfony/gmltowkt", // replace this url with geoviz lib
             type: "POST",
             data: {gml: GML},
             context: document.body
@@ -760,7 +760,7 @@ function GeoViz()
     this.wktToGML = function (WKT)
     {
         jQuery.ajax({
-            url: "includes/geoviz/wkttogml.php", // replace this url with geoviz lib
+            url: "pelagos-symfony/wkttogml", // replace this url with geoviz lib
             type: "POST",
             data: {wkt: WKT},
             context: document.body
